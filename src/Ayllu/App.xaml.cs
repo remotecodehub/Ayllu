5e91769fb7e11bb20ce4ba504397fd604b9a2046
Weslley Murdock <weslleyluiz@outlook.com.br>
2025-08-19 04:28:27 -0300

    removed and recreated projects in .net 8.0

---
+++ src/Ayllu/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ayllu
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
